8cd56c8371e95f4d3ea870595e49718b72328624
prashantsaini306 <prashantsaini306@gmail.com>
2025-09-19 22:14:31 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import re
 
-st.title("Weather Gradient Viewer")
+st.title("Weather Gradient Tracker")
 
 # ---- Dropdown menu ----
 parameter = st.selectbox(
